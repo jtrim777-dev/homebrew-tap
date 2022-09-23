a66a56e380467ba9949074f38ce26c523e0c6e97
Jtrim777 <jatrimble777@gmail.com>
2022-09-23 15:39:10 -0700

    Update the jvmutil.rb formula to 0.1.3

--- Formula/jvmutil.rb
+++ Formula/jvmutil.rb
@@ -3,9 +3,9 @@ class Jvmutil < Formula
 
   desc "A utility for viewing and managing the currently installed and active JVMs on macOS"
   homepage "https://github.com/jtrim777-dev/jvmutil"
-  url "https://maven.jtrim777.dev/releases/dev/jtrim777/jvmutil/0.1.2/jvmutil-pkg.tgz"
-  version "0.1.2"
-  sha256 "dd450b798e22c46321f075eb889a0fc3c78108d1bf6fe72235e0ff38cd150665"
+  url "https://maven.jtrim777.dev/releases/dev/jtrim777/jvmutil/0.1.3/jvmutil-pkg.tgz"
+  version "0.1.3"
+  sha256 "a13f54863950fccf32c9615035cd6e0339df6f8bae54f0f7dd7467ecc8310f9b"
   license "MIT"
 
   depends_on "python"
@@ -13,7 +13,7 @@ class Jvmutil < Formula
   def install
     bin.install Dir["bin/*"]
     man1.install Dir["share/man/man1/*"]
-    chmod "ugo=rwx", bin/"jvmutil", verbose: true
+    # chmod "ugo=rwx", bin/"jvmutil", verbose: true
 
     executable = (bin/"jvmutil").to_s
     system executable, "relink"
