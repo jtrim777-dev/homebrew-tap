e3ee8c89b6f1b581a9ce514e410d7a55612ac576
Jtrim777 <jatrimble777@gmail.com>
2022-09-23 15:25:53 -0700

    Update the jvmutil.rb formula to 0.1.2

--- Formula/jvmutil.rb
+++ Formula/jvmutil.rb
@@ -3,34 +3,25 @@ class Jvmutil < Formula
 
   desc "A utility for viewing and managing the currently installed and active JVMs on macOS"
   homepage "https://github.com/jtrim777-dev/jvmutil"
-  url "https://maven.jtrim777.dev/releases/dev/jtrim777/jvmutil/0.1.1/jvmutil-pkg.tgz"
-  version "0.1.1"
-  sha256 "296d677ccac20d7069d62d215ebeb70e10d89c5cc89037297ad029c2b197f0b5"
+  url "https://maven.jtrim777.dev/releases/dev/jtrim777/jvmutil/0.1.2/jvmutil-pkg.tgz"
+  version "0.1.2"
+  sha256 "ecbdb6d48c268ce7cb560f9d731f7bad7bb952a4b935eafa326d3f9b53d06506"
   license "MIT"
 
   depends_on "python"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
     # virtualenv_install_with_resources
     bin.install Dir["bin/*"]
     man1.install Dir["share/man/man1/*"]
     chmod "ugo=rwx", bin/"jvmutil", verbose: true
 
-    output = system bin/"jvmutil", "-q", "active", "--fields", "path", "--display", "raw"
-    ln_s libexec/"javahome", output
-  end
+    executable = (bin/"jvmutil").to_s
+    # gpo = `#{executable} -q active --fields path --display raw`
+    system executable, "relink"
 
-  # test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test jvmutil`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    # system "false"
-  # end
+    # mkdir libexec
+    # ln_s gpo, libexec/"javahome"
+    libexec.install_symlink gpo
+  end
 end
