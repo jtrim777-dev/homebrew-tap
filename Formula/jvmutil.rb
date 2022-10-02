c2ad0424875c22b587b832fe556f01a21d1a943c
Jtrim777 <jatrimble777@gmail.com>
2022-10-02 04:20:09 +0000

    Update the jvmutil.rb formula to 0.1.9

--- Formula/jvmutil.rb
+++ Formula/jvmutil.rb
@@ -3,9 +3,9 @@ class Jvmutil < Formula
 
   desc "A utility for viewing and managing the currently installed and active JVMs on macOS"
   homepage "https://github.com/jtrim777-dev/jvmutil"
-  url "https://maven.jtrim777.dev/snapshots/dev/jtrim777/jvmutil/0.1.9-alpha.9/jvmutil-pkg.tgz"
-  version "0.1.9-alpha.9"
-  sha256 "8c315ebe0dda91fa009b20abbb4aab74a741ac8b7b9c3868c3fa6c74cd64e758"
+  url "https://maven.jtrim777.dev/snapshots/dev/jtrim777/jvmutil/0.1.9/jvmutil-pkg.tgz"
+  version "0.1.9"
+  sha256 "a9740bd63e1143bfe9de275734b1130be468efe40d69acfacda9a9b14eca71e2"
   license "MIT"
 
   depends_on "python"
